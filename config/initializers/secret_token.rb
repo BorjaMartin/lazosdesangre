8635a8e26554a3ac1d0e9ed7603e73e2c638d2ea
Borja Martin Fernandez <bmartinf@axpe.com>
2013-09-30 09:14:35 +0200

    Rails new lazosdesangre

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lazosdesangre::Application.config.secret_key_base = '319d5bb4d0abd34428e141a242821597a52f1a878165f28d8a9a4301f2b718f5ed8719848c84949c2c8938bbe1d9fb625c7be653bbb9fc9b2e5af2bc71260b73'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Lazosdesangre::Application.config.session_store :cookie_store, key: '_lazosdesangre_session'
